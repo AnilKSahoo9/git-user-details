7581d01f80de3c7acefbda38ff09c00690f533d4
AnilKSahoo <asahoo112@gmail.com>
2020-07-09 23:25:58 +0530

    Created action creator for holding fetched data of repo,commit,contribution

--- src/redux/ActionCreate.ts
+++ src/redux/ActionCreate.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
-import { actionType, REQUEST_USER_DETAILS, RECEIVE_USER_DETAILS } from "./Type";
+import {
+  actionType,
+  REQUEST_USER_DETAILS,
+  RECEIVE_USER_DETAILS,
+  RECEIVE_REPO_DETAILS,
+  RECEIVE_COMMIT_DETAILS,
+  RECEIVE_CONTRIBUTION_DETAILS,
+} from "./Type";
 import { ThunkAction } from "redux-thunk";
 import { Action } from "redux";
 import { ActionCreator } from "redux";
@@ -12,16 +19,75 @@ export const getUser: ActionCreator<actionType> = (event) => {
   };
 };
 
+export const getRepos = (userInput: string) => async (dispatch: any) => {
+  const responseRepo = await axios.get(
+    `https://api.github.com/users/${userInput}/repos?sort=updated`
+  );
+  //console.log(responseRepo.data);
+  dispatch({
+    type: RECEIVE_REPO_DETAILS,
+    data: responseRepo.data,
+  });
+  const repoArray = responseRepo.data.map((item: any) => item.name);
+  //console.log(repoArray);
+  for (let i = 0; i < repoArray.length; i++) {
+    //console.log(repoArray[i]);
+    dispatch(getCommits(userInput, repoArray[i], i));
+  }
+};
+
+const arrayCommit: any[] = [];
+const getCommits = (userInput: string, repoName: string, i: number) => async (
+  dispatch: any
+) => {
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+  const responseCommit = await axios.get(
+    `https://api.github.com/repos/${userInput}/${repoName}/commits?sort=updated`,
+    { headers }
+  );
+  //console.log(responseCommit.data);
+  //console.log(i);
+  arrayCommit[i] = responseCommit.data.length;
+  //console.log(array1.length);
+  //console.log(array1);
+  const totalCommit = arrayCommit.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  //console.log(totalCommit);
+
+  dispatch({
+    type: RECEIVE_COMMIT_DETAILS,
+    data: totalCommit,
+  });
+};
+
+const getContributions = (userInput: string) => async (dispatch: any) => {
+  const responseContributions = await axios.get(
+    `https://github-contributions-api.now.sh/v1/${userInput}`
+  );
+  const contributionData = responseContributions.data.years;
+  dispatch({
+    type: RECEIVE_CONTRIBUTION_DETAILS,
+    data: contributionData,
+  });
+  console.log(contributionData);
+};
+
 export const getData = (
   userInput: string,
   event: any
 ): ThunkAction<void, appState, null, Action<string>> => async (dispatch) => {
   event.preventDefault();
-  const res = await axios.get(`https://api.github.com/users/${userInput}`);
-  console.log("data fetched");
+  const responseData = await axios.get(
+    `https://api.github.com/users/${userInput}`
+  );
+  //console.log("action data fetched");
   dispatch({
     type: RECEIVE_USER_DETAILS,
-    data: res.data,
+    data: responseData.data,
     event: event,
   });
+  dispatch(getContributions(userInput));
 };
