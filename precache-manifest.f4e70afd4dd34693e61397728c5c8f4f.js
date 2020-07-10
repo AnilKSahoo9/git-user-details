a73268e430904faf78cbe3c88d74865d76374c15
AnilKSahoo <asahoo112@gmail.com>
2020-07-10 10:00:41 +0530

    Updates

---
+++ precache-manifest.f4e70afd4dd34693e61397728c5c8f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b6c8b7cba057239e5b4f772e428e60",
+    "url": "/git-user-details/index.html"
+  },
+  {
+    "revision": "11b396150bfc57527728",
+    "url": "/git-user-details/static/css/main.745c06fd.chunk.css"
+  },
+  {
+    "revision": "4ec4513e082ff391b51b",
+    "url": "/git-user-details/static/js/2.36c678f8.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/git-user-details/static/js/2.36c678f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b396150bfc57527728",
+    "url": "/git-user-details/static/js/main.88008666.chunk.js"
+  },
+  {
+    "revision": "c208fa6a434364a7bfab",
+    "url": "/git-user-details/static/js/runtime-main.3e20a37c.js"
+  }
+]);
